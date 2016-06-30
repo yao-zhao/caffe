15ea1754d419e42e223c9a1a6f50bb43d40ffa72
Yao <jimzhaoshine@gmail.com>
2016-06-30 16:41:07 -0400

    modified batch norm test to test for param

--- src/caffe/test/test_batch_norm_layer.cpp
+++ src/caffe/test/test_batch_norm_layer.cpp
@@ -22,18 +22,42 @@ namespace caffe {
     typedef typename TypeParam::Dtype Dtype;
    protected:
     BatchNormLayerTest()
-        : blob_bottom_(new Blob<Dtype>(5, 2, 3, 4)),
-          blob_top_(new Blob<Dtype>()) {
+        : blob_bottom_(new Blob<Dtype>(4, 3, 2, 5)),
+          blob_top_(new Blob<Dtype>()),
+          blob_bottom2_(new Blob<Dtype>()),
+          blob_param_(new Blob<Dtype>()) {
       // fill the values
       FillerParameter filler_param;
       GaussianFiller<Dtype> filler(filler_param);
       filler.Fill(this->blob_bottom_);
       blob_bottom_vec_.push_back(blob_bottom_);
       blob_top_vec_.push_back(blob_top_);
+      int num = this->blob_bottom_->num();
+      int channels = this->blob_bottom_->channels();
+      int height = this->blob_bottom_->height();
+      int width = this->blob_bottom_->width();
+      blob_bottom2_->ReshapeLike(*blob_bottom_);
+      // fill the values
+      for (int j = 0; j < channels; ++j) {
+        int index =0;
+        for (int i = 0; i < num; ++i) {
+          for ( int k = 0; k < height; ++k ) {
+            for ( int l = 0; l < width; ++l ) {
+              index++;
+              this->blob_bottom2_->mutable_cpu_data()
+              [this->blob_bottom2_->offset(i, j, k, l)] 
+              = Dtype(2*(j+3) + 2*(index%2)-1);
+            }
+          }
+        }
+      }
     }
-    virtual ~BatchNormLayerTest() { delete blob_bottom_; delete blob_top_; }
+    virtual ~BatchNormLayerTest() { delete blob_bottom_; delete blob_top_; 
+      delete blob_bottom2_; delete blob_param_; }
     Blob<Dtype>* const blob_bottom_;
     Blob<Dtype>* const blob_top_;
+    Blob<Dtype>* const blob_bottom2_;
+    Blob<Dtype>* const blob_param_;
     vector<Blob<Dtype>*> blob_bottom_vec_;
     vector<Blob<Dtype>*> blob_top_vec_;
   };
@@ -76,9 +100,55 @@ namespace caffe {
     }
   }
 
+  TYPED_TEST(BatchNormLayerTest, TestTwoTopsForward) {
+    typedef typename TypeParam::Dtype Dtype;
+    LayerParameter layer_param;
+    BatchNormParameter* batch_norm_param = layer_param.mutable_batch_norm_param();
+    batch_norm_param->set_use_global_stats(false);
+    this->blob_top_vec_.push_back(this->blob_param_);
+
+    BatchNormLayer<Dtype> layer(layer_param);
+    this->blob_bottom_vec_[0] = this->blob_bottom2_;
+    layer.SetUp(this->blob_bottom_vec_, this->blob_top_vec_);
+    layer.Forward(this->blob_bottom_vec_, this->blob_top_vec_);
+    this->blob_bottom_vec_[0] = this->blob_bottom_;
+
+    // Test mean
+    int num = this->blob_bottom_->num();
+    int channels = this->blob_bottom_->channels();
+    int height = this->blob_bottom_->height();
+    int width = this->blob_bottom_->width();
+
+    for (int j = 0; j < channels; ++j) {
+      Dtype sum = 0, var = 0;
+      for (int i = 0; i < num; ++i) {
+        for ( int k = 0; k < height; ++k ) {
+          for ( int l = 0; l < width; ++l ) {
+            Dtype data = this->blob_top_->data_at(i, j, k, l);
+            sum += data;
+            var += data * data;
+          }
+        }
+      }
+      sum /= height * width * num;
+      var /= height * width * num;
+
+      const Dtype kErrorBound = 0.1;
+      // expect zero mean
+      EXPECT_NEAR(0, sum, kErrorBound);
+      // expect unit variance
+      EXPECT_NEAR(1, var, kErrorBound);
+      // expect  mean and var
+      const Dtype* batch_norm_data = this->blob_top_vec_[1]->cpu_data();
+      EXPECT_NEAR(2*(j+3), batch_norm_data[j], kErrorBound);
+      EXPECT_NEAR(1, batch_norm_data[j+channels], kErrorBound);
+    }
+  }
+
   TYPED_TEST(BatchNormLayerTest, TestForwardInplace) {
     typedef typename TypeParam::Dtype Dtype;
-    Blob<Dtype> blob_inplace(5, 2, 3, 4);
+    Blob<Dtype> blob_inplace;
+    blob_inplace.ReshapeLike(*this->blob_bottom_);
     vector<Blob<Dtype>*> blob_bottom_vec;
     vector<Blob<Dtype>*> blob_top_vec;
     LayerParameter layer_param;
