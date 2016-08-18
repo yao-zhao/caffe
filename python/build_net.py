3ae6b0f8d6c2bd3fd3d197c1465870e2d2915c09
Yao Zhao <jimzhaoshine@gmail.com>
2016-08-18 01:23:24 -0400

    tmp commitment

--- python/build_net.py
+++ python/build_net.py
@@ -2,23 +2,20 @@
 """
 collection of quick helper to build layers
 """
+
 import os
 import caffe
 from caffe import layers as L, params as P
 from caffe.proto import caffe_pb2
+import numpy as np
 
 class BuildNet:
     # ini def
     def __init__(self, func, name = 'net', savepath = 'models/'):
-        self.index = 1
-        self.bottom = None
-        self.label = None
-        self.net = caffe.NetSpec()
-        self.phase = 'train'
         self.func = func
-        self.solver = None
         self.model_path = savepath + name +'/'
         self.name = name
+        self.reset()
 
 # common task
 ################################################################################
@@ -38,6 +35,20 @@ class BuildNet:
         self.index = 1
         self.net = None
         self.net = caffe.NetSpec()
+        self.solvers = []
+        self.stage = None
+        self.stage_iters = []
+        self.bottom = None
+        self.label = None
+        self.phase = 'train'
+
+    def number_stages(self):
+        return len(self.solvers)
+
+    # check stage
+    def check_stage(self, stage):
+        return not stage or not self.stage or \
+                np.any(np.equal(stage, self.stage))
 
 # function groups
 ################################################################################
@@ -167,34 +178,37 @@ class BuildNet:
 
 # output layers
 ################################################################################
-    def add_softmax(self, loss_weight = 1, name = 'softmax'):
-        if self.phase == 'train' or self.phase == 'test':
-            softmax = L.SoftmaxWithLoss(self.bottom, self.label,
-                                        loss_weight = loss_weight)
-            accuracy = L.Accuracy(self.bottom, self.label)
-            setattr(self.net, name+'loss', softmax)
-            setattr(self.net, 'accuracy', accuracy)
-        elif self.phase == 'deploy':
-            softmax = L.Softmax(self.bottom)
-            setattr(self.net, name+'prob', softmax)
+    def add_softmax(self, loss_weight = 1, name = 'softmax', stage = None):
+        if self.check_stage(stage):
+            if self.phase == 'train' or self.phase == 'test':
+                softmax = L.SoftmaxWithLoss(self.bottom, self.label,
+                                            loss_weight = loss_weight)
+                accuracy = L.Accuracy(self.bottom, self.label)
+                setattr(self.net, name+'loss', softmax)
+                setattr(self.net, 'accuracy', accuracy)
+            elif self.phase == 'deploy':
+                softmax = L.Softmax(self.bottom)
+                setattr(self.net, name+'prob', softmax)
 
     # add softmax
-    def add_euclidean(self, loss_weight = 1, name = 'euclidean'):
-        if self.phase == 'train' or self.phase == 'test':
-            euclidean = L.EuclideanLoss(self.bottom, self.label,
-                                      loss_weight = loss_weight)
-            setattr(self.net, name+'loss', euclidean)
+    def add_euclidean(self, loss_weight = 1, name = 'euclidean', stage = None):
+        if self.check_stage(stage):
+            if self.phase == 'train' or self.phase == 'test':
+                euclidean = L.EuclideanLoss(self.bottom, self.label,
+                                          loss_weight = loss_weight)
+                setattr(self.net, name+'loss', euclidean)
 
     # add gaussian prob loss layer
     def add_gaussian_prob(self, mean, var, label = None,
-        eps = 1e-2, loss_weight = 1, name = 'gaussianprob'):
-        if not label:
-            label = self.label
-        if self.phase == 'train' or self.phase == 'test':
-            gaussianprob = L.GaussianProbLoss(mean, var, label,
-                gaussian_prob_loss_param = dict(eps = eps),
-                loss_weight = loss_weight)
-            setattr(self.net, name+'loss', gaussianprob)
+        eps = 1e-2, loss_weight = 1, name = 'gaussianprob', stage = None):
+        if self.check_stage(stage):
+            if not label:
+                label = self.label
+            if self.phase == 'train' or self.phase == 'test':
+                gaussianprob = L.GaussianProbLoss(mean, var, label,
+                    gaussian_prob_loss_param = dict(eps = eps),
+                    loss_weight = loss_weight)
+                setattr(self.net, name+'loss', gaussianprob)
 
     # depreciated function
     # # add gaussian prob loss layer with fc
@@ -286,9 +300,10 @@ class BuildNet:
         return self.bottom
 
     # ReLU 
-    def add_relu(self):
-        self.bottom = L.ReLU(self.bottom, in_place = True)
-        setattr(self.net, 'relu'+str(self.index), self.bottom)
+    def add_relu(self, stage = None):
+        if self.check_stage(stage):
+            self.bottom = L.ReLU(self.bottom, in_place = True)
+            setattr(self.net, 'relu'+str(self.index), self.bottom)
         return self.bottom
 
     # Parameterized ReLU 
@@ -300,23 +315,24 @@ class BuildNet:
 
     # add fc
     def add_fc(self, num_output, lr = 1, dropout = 0, bias_value = 0,
-            weight_filler = 'xavier', name = None):
-        if not name:
-            name = 'fc'+str(self.index)
-        if self.phase == 'train' or self.phase == 'test':
-            self.bottom = L.InnerProduct(self.bottom,
-                num_output = num_output,
-                param = [dict(lr_mult = lr), dict(lr_mult = lr)],
-                weight_filler = dict(type = weight_filler),
-                bias_filler = dict(type = 'constant', value = bias_value))
-        elif self.phase == 'deploy':
-            self.bottom = L.InnerProduct(self.bottom,
-                num_output = num_output)
-        setattr(self.net, name, self.bottom)
-        if dropout > 0:
-            self.bottom = L.Dropout(self.bottom, dropout_ratio = dropout,
-                in_place = True)
-            setattr(self.net, 'dropout'+str(self.index), self.bottom)
+            weight_filler = 'xavier', name = None, stage = None):
+        if self.check_stage(stage):
+            if not name:
+                name = 'fc'+str(self.index)
+            if self.phase == 'train' or self.phase == 'test':
+                self.bottom = L.InnerProduct(self.bottom,
+                    num_output = num_output,
+                    param = [dict(lr_mult = lr), dict(lr_mult = lr)],
+                    weight_filler = dict(type = weight_filler),
+                    bias_filler = dict(type = 'constant', value = bias_value))
+            elif self.phase == 'deploy':
+                self.bottom = L.InnerProduct(self.bottom,
+                    num_output = num_output)
+            setattr(self.net, name, self.bottom)
+            if dropout > 0:
+                self.bottom = L.Dropout(self.bottom, dropout_ratio = dropout,
+                    in_place = True)
+                setattr(self.net, 'dropout'+str(self.index), self.bottom)
         self.index += 1
         return self.bottom
 
@@ -343,43 +359,54 @@ class BuildNet:
 # solvers
 ################################################################################
     # define sdg solver
-    def set_solver_sdg(self, test_interval = 100, test_iter = 1,
+    def add_solver_sdg(self, test_interval = 100, test_iter = 1,
                 max_iter = 6e3, base_lr = 0.01, momentum = 0.9,
                 weight_decay = 1e-5, gamma = 0.1, stepsize = 2e3,
                 display = 10, snapshot = 1e3):
-        self.solver = None
-        self.solver = caffe_pb2.SolverParameter()
-        # self.solver.random_seed = 0xCAFFE
-        self.solver.train_net = self.model_path+'train.prototxt'
-        self.solver.test_net.append(self.model_path+'test.prototxt')
-        self.solver.test_interval = test_interval
-        self.solver.test_iter.append(test_iter)
-        self.solver.max_iter = int(max_iter)
-        self.solver.type = "SGD"
-        self.solver.base_lr = base_lr
-        self.solver.momentum = momentum
-        self.solver.weight_decay = weight_decay
-        self.solver.lr_policy = 'step'
-        self.solver.gamma = gamma
-        self.solver.stepsize  = int(stepsize)
-        self.solver.display = int(display)
-        self.solver.snapshot = int(snapshot)
-        self.solver.snapshot_prefix = self.model_path+self.name
-        self.solver.solver_mode = caffe_pb2.SolverParameter.GPU
+        solver = caffe_pb2.SolverParameter()
+        # solver.random_seed = 0xCAFFE
+        solver.train_net = self.model_path+'train_'+\
+                str(self.number_stages())+'.prototxt'
+        solver.test_net.append(self.model_path+'test_'+\
+                str(self.number_stages())+'.prototxt')
+        solver.test_interval = test_interval
+        solver.test_iter.append(test_iter)
+        solver.max_iter = int(max_iter)
+        solver.type = "SGD"
+        solver.base_lr = base_lr
+        solver.momentum = momentum
+        solver.weight_decay = weight_decay
+        solver.lr_policy = 'step'
+        solver.gamma = gamma
+        solver.stepsize  = int(stepsize)
+        solver.display = int(display)
+        solver.snapshot = int(snapshot)
+        solver.snapshot_prefix = self.model_path+'stage_'+\
+                str(self.number_stages())
+        solver.solver_mode = caffe_pb2.SolverParameter.GPU
+        self.solvers.append(solver)
+        self.stage_iters.append(solver.max_iter)
 
 # saving to files
 ################################################################################
     # write solver
     def save_solver(self):
-        if self.solver:
-            self.solver.test_initialization = False
-            with open(self.model_path+'solver.prototxt', 'w+') as f:
-                f.write(str(self.solver))
-            self.solver.test_initialization = True
-            self.solver.max_iter = 1
-            with open(self.model_path+'solver_preload.prototxt', 'w+') as f:
-                f.write(str(self.solver))
-            print self.name+': '+'solver writing finished'
+        self.reset()
+        self.func(self)
+        for solver, stage in \
+                zip(self.solvers, range(self.number_stages())):
+            # training solver
+            solver.test_initialization = False
+            with open(self.model_path+'solver_'
+                    +str(stage)+'.prototxt', 'w+') as f:
+                f.write(str(solver))
+            # pre-checking solver
+            solver.test_initialization = True
+            solver.max_iter = 1
+            with open(self.model_path+'solver_checking_'
+                    +str(stage)+'.prototxt', 'w+') as f:
+                f.write(str(solver))
+            print self.name+': '+'solver stage '+str(stage)+' writing finished'
 
     # save net
     def save_net(self):
@@ -389,21 +416,84 @@ class BuildNet:
             self.reset()
             self.phase = phase
             self.func(self)
-            with open(self.model_path+self.phase+'.prototxt', 'w+') as f:
-                f.write('name: \"'+self.name+'\"\n')
-                if phase == 'deploy':
-                    numbatch, numchannel, height, width = caffe.Net(
-                        self.model_path+'train.prototxt',
-                        caffe.TRAIN).blobs['data'].shape
-                    f.write('input: \"data\"\n')
-                    f.write('input_dim: '+str(numbatch)+'\n')
-                    f.write('input_dim: '+str(numchannel)+'\n')
-                    f.write('input_dim: '+str(height)+'\n')
-                    f.write('input_dim: '+str(width)+'\n')
-                f.write(str(self.net.to_proto() ))
-                print self.name+': '+self.phase+' net writing finished!'
+            for stage in range(self.number_stages()):
+                self.stage  = stage
+                with open(self.model_path+self.phase+'_'
+                        +str(stage)+'.prototxt', 'w+') as f:
+                    f.write('name: \"'+self.name+'\"\n')
+                    if phase == 'deploy':
+                        numbatch, numchannel, height, width = caffe.Net(
+                            self.model_path+'train_0.prototxt',
+                            caffe.TRAIN).blobs['data'].shape
+                        f.write('input: \"data\"\n')
+                        f.write('input_dim: '+str(numbatch)+'\n')
+                        f.write('input_dim: '+str(numchannel)+'\n')
+                        f.write('input_dim: '+str(height)+'\n')
+                        f.write('input_dim: '+str(width)+'\n')
+                    f.write(str(self.net.to_proto() ))
+                    print self.name+': stage '+str(stage)+' '+ \
+                            self.phase+' net writing finished!'
+
+    # generate runfile header
+    def gen_runfile_header(self, f):
+        f.write('GPU=0\n'+
+                'REPEAT=1\n'
+                'while [[ $# -gt 1 ]]\n'+
+                'do\n'+
+                'key="$1"\n'+
+                'case $key in\n'+
+                '-g|--gpu)\n'+
+                'GPU="$2"\n'+
+                'shift # past argument\n'+
+                ';;\n'+
+                '-r|--repeat)\n'+
+                'REPEAT="$2"\n'+
+                'shift # past argument\n'+
+                ';;\n'+
+                '*)\n'+
+                '# unknown option'
+                ';;\n'+
+                'esac\n'+
+                'shift # past argument\n'+
+                'done\n')
+
+    # save runfile
+    def save_runfile(self):
+        with open(self.model_path+'runfile.sh', 'w+') as f:
+            self.gen_runfile_header(f)
+            self.reset()
+            self.func(self)
+            for stage in range(self.number_stages()):
+                f.write('~/caffe-yao/build/tools/caffe train -gpu $GPU'+
+                        ' \\\n--solver=models/'+self.name+
+                        '/solver_'+str(stage)+'.prototxt')
+                if stage > 0:
+                    f.write(' \\\n--weights=models/'+self.name+
+                            '/stage_'+str(stage)+'_iter_'+
+                            str(self.stage_iters[stage-1])+'.solverstate')
+                f.write(' \\\n2>&1 | tee ')
+                if stage > 0:
+                    f.write('-a ')
+                f.write('models/'+self.name+'/log_$REPEAT.txt\n')
+
+    # save runfile
+    def save_checking(self):
+        with open(self.model_path+'checking.sh', 'w+') as f:
+            self.gen_runfile_header(f)
+            self.reset()
+            self.func(self)
+            for stage in range(self.number_stages()):
+                f.write('~/caffe-yao/build/tools/caffe train -gpu $GPU'+
+                        ' \\\n--solver=models/'+self.name+
+                        '/solver_checking_'+str(stage)+'.prototxt')
+                f.write(' \\\n2>&1 | tee ')
+                if stage > 0:
+                    f.write('-a ')
+                f.write('models/'+self.name+'/log_checking_$REPEAT.txt\n')
 
     # save
     def save(self):
-        self.save_net()
+        # self.save_net()
         self.save_solver()
+        self.save_runfile()
+        self.save_checking()
