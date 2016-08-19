f0a2581af1d23c8a5eb41593d8aa3da3178e0695
Yao Zhao <jimzhaoshine@gmail.com>
2016-08-19 01:18:28 -0400

    fixed ifdef USE_OPENCV location

--- include/caffe/util/image_transformations.hpp
+++ include/caffe/util/image_transformations.hpp
@@ -1,6 +1,5 @@
 #ifndef CAFFE_UTIL_IMAGE_TRANSFORMATIONS_H_
 #define CAFFE_UTIL_IMAGE_TRANSFORMATIONS_H_
-
 #ifdef USE_OPENCV
 
 #include <opencv2/core/core.hpp>
@@ -16,8 +15,7 @@ void RandomRotateImage(const cv::Mat& src_img, const int rotation_range,
 void RandomPerspectiveTransformImage(const cv::Mat& src_img,
     const int perspective_transformation_border, cv::Mat* dst_img);
 
-#endif  // USE_OPENCV
-
 }  // namespace caffe
 
+#endif  // USE_OPENCV
 #endif  // CAFFE_UTIL_IMAGE_TRANSFORMATIONS_H_
